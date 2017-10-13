797b696bcb7a69171b802d60a17d1406812f3328
zabeen <zabeen.patel@ucl.ac.uk>
2017-10-13 17:47:48 +0100

    Added code to move Bishop diagonally (passed unit test 04)

--- Chessington.GameEngine/Pieces/Bishop.cs
+++ Chessington.GameEngine/Pieces/Bishop.cs
@@ -10,7 +10,60 @@ namespace Chessington.GameEngine.Pieces
 
         public override IEnumerable<Square> GetAvailableMoves(Board board)
         {
-            return Enumerable.Empty<Square>();
+            // returned list
+            List<Square> availablePositions = new List<Square>();
+
+            // get current position
+            Square currentPosition = board.FindPiece(this);
+
+            // set row numbers
+            int rowAbove = currentPosition.Row - 1;
+            int rowBelow = currentPosition.Row + 1;
+
+            // move from right of current position to right edge of board
+            for (int col = currentPosition.Col + 1; col < 8; col++)
+            {
+                // add new squares until top row is reached
+                if (rowAbove > -1)
+                {
+                    availablePositions.Add(new Square(rowAbove, col));
+                    rowAbove--;
+                }
+
+                // add new squares until bottom row is reached
+                if (rowBelow < 8)
+                {
+                    availablePositions.Add(new Square(rowBelow, col));
+                    rowBelow++;
+                }
+            }
+
+            // reset row numbers
+            rowAbove = currentPosition.Row - 1;
+            rowBelow = currentPosition.Row + 1;
+
+            // move from left of current position to left edge of board
+            for (int col = currentPosition.Col - 1; col > -1; col--)
+            {
+                // add new squares until top row is reached
+                if (rowAbove > -1)
+                {
+                    availablePositions.Add(new Square(rowAbove, col));
+                    rowAbove--;
+                }
+
+                // add new squares until bottom row is reached
+                if (rowBelow < 8)
+                {
+                    availablePositions.Add(new Square(rowBelow, col));
+                    rowBelow++;
+                }
+            }
+
+            //remove squares that match starting/ current position
+            availablePositions.RemoveAll(a => a == currentPosition);
+
+            return availablePositions;
         }
     }
 }
