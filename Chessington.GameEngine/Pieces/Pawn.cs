bc668460f8855c20c66609586b3832d9432408c8
SamWatts <sam_watts89@hotmail.co.uk>
2017-10-13 12:21:14 +0100

    passed first unit test

--- Chessington.GameEngine/Pieces/Pawn.cs
+++ Chessington.GameEngine/Pieces/Pawn.cs
@@ -10,7 +10,23 @@ namespace Chessington.GameEngine.Pieces
 
         public override IEnumerable<Square> GetAvailableMoves(Board board)
         {
-            return Enumerable.Empty<Square>();
+            // returned list
+            List<Square> availableMoves = new List<Square>();
+ 
+            // find where pawn is on board
+            Square currentPosition = board.FindPiece(this);
+
+            // Change row according player colour
+            int newRow = (Player == Player.White) ? currentPosition.Row - 1 : currentPosition.Row + 1;
+
+            // create new position
+            Square newPosition = new Square(newRow, currentPosition.Col);
+
+            // Add move to list
+            availableMoves.Add(newPosition);
+
+            // return list of available moves
+            return availableMoves;
         }
     }
 }
