87e0d6d8a61523df47b07e10602b60fe44e7c519
zabeen <zabeen.patel@ucl.ac.uk>
2017-10-13 23:45:52 +0100

    Moved diagonal method from Bishop class to parent Piece class, so it can be accessed by other child classes

--- Chessington.GameEngine/Pieces/Bishop.cs
+++ Chessington.GameEngine/Pieces/Bishop.cs
@@ -10,42 +10,7 @@ namespace Chessington.GameEngine.Pieces
 
         public override IEnumerable<Square> GetAvailableMoves(Board board)
         {
-            // returned list
-            List<Square> availablePositions = new List<Square>();
-
-            // get current position
-            Square curr = board.FindPiece(this);
-
-            // Row values at left and right edges of board
-            int topLeft = curr.Row - curr.Col;
-            int topRight = curr.Row + curr.Col - 7;
-            int bottomLeft = curr.Row + curr.Col;
-            int bottomRight = curr.Row - curr.Col + 7;
-
-            // add squares top left to bottom right
-            AddSquareForEachColAtSpecifiedRows(
-                ref availablePositions,
-                rows: Enumerable.Range(topLeft, bottomRight - topLeft + 1).ToList());
-
-            // add squares bottom left to top right
-            AddSquareForEachColAtSpecifiedRows(
-                ref availablePositions,
-                rows: Enumerable.Range(topRight, bottomLeft - topRight + 1).Reverse().ToList());
-
-            //remove squares that match current position
-            availablePositions.RemoveAll(a => a == curr);
-
-            return availablePositions;
-        }
-
-        private void AddSquareForEachColAtSpecifiedRows (ref List<Square> availablePositions, List<int> rows)
-        {            
-            for (int col = 0; col < 8; col++)
-            {
-                // only add square if row value is within board
-                if (rows[col] >= 0 && rows[col] < 8)
-                    availablePositions.Add(Square.At(rows[col], col));
-            }
+            return GetAvailableMoves_Diagonal(board.FindPiece(this));
         }
     }
 }
--- Chessington.GameEngine/Pieces/Piece.cs
+++ Chessington.GameEngine/Pieces/Piece.cs
@@ -6,12 +6,12 @@ namespace Chessington.GameEngine.Pieces
 {
     public abstract class Piece
     {
+        public Player Player { get; private set; }
+
         protected Piece(Player player)
         {
             Player = player;
         }
-
-        public Player Player { get; private set; }
 
         public abstract IEnumerable<Square> GetAvailableMoves(Board board);
 
@@ -19,6 +19,43 @@ namespace Chessington.GameEngine.Pieces
         {
             var currentSquare = board.FindPiece(this);
             board.MovePiece(currentSquare, newSquare);
+        }
+
+        protected IEnumerable<Square> GetAvailableMoves_Diagonal(Square current)
+        {
+            // returned list
+            List<Square> availablePositions = new List<Square>();
+
+            // Row values at left and right edges of board
+            int topRowLeftEdge = current.Row - current.Col;
+            int bottomRowRightEdge = topRowLeftEdge + 7;
+            int bottomRowLeftEdge = current.Row + current.Col;
+            int topRowRightEdge = bottomRowLeftEdge - 7;
+
+            // add squares top left to bottom right
+            AddSquareForEachColAtSpecifiedRows(
+                ref availablePositions,
+                rows: Enumerable.Range(topRowLeftEdge, bottomRowRightEdge - topRowLeftEdge + 1).ToList());
+
+            // add squares bottom left to top right
+            AddSquareForEachColAtSpecifiedRows(
+                ref availablePositions,
+                rows: Enumerable.Range(topRowRightEdge, bottomRowLeftEdge - topRowRightEdge + 1).Reverse().ToList());
+
+            //remove squares that match current position
+            availablePositions.RemoveAll(a => a == current);
+
+            return availablePositions;
+        }
+
+        private void AddSquareForEachColAtSpecifiedRows(ref List<Square> availablePositions, List<int> rows)
+        {
+            for (int col = 0; col < 8; col++)
+            {
+                // only add square if row value is within board
+                if (rows[col] >= 0 && rows[col] < 8)
+                    availablePositions.Add(Square.At(rows[col], col));
+            }
         }
     }
 }
