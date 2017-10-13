db1df6905af6f15060a247c1f47d2e7667362588
SamWatts <sam_watts89@hotmail.co.uk>
2017-10-13 13:48:37 +0100

    unit test 3 complete

--- Chessington.GameEngine/Pieces/Rook.cs
+++ Chessington.GameEngine/Pieces/Rook.cs
@@ -10,7 +10,24 @@ namespace Chessington.GameEngine.Pieces
 
         public override IEnumerable<Square> GetAvailableMoves(Board board)
         {
-            return Enumerable.Empty<Square>();
+            // returned list
+            List<Square> availablePositions = new List<Square>();
+
+            // get current position
+            Square currentPosition = board.FindPiece(this);
+          
+            for (int i = 0; i < 8; i++)
+            {
+                //create new squares where row remains const and col iterates from 0 - 7
+                availablePositions.Add(new Square(currentPosition.Row, i));
+                //create new squares where column remains const and row iterates from 0 - 7
+                availablePositions.Add(new Square(i, currentPosition.Col));
+            }
+
+            //remove squares that match starting/ current position
+            availablePositions.RemoveAll(a => a == currentPosition);
+
+            return availablePositions;
         }
     }
 }
