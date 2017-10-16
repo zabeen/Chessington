f9e51acd217259952308a8c451bb3c60e010c472
zabeen <zabeen.patel@ucl.ac.uk>
2017-10-16 10:32:30 +0100

    added new test for availablemoves to check whether all squares are in bounds

---
+++ Chessington.GameEngine.Tests/AvailableMovesTest.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using NUnit.Framework;
+using Chessington.GameEngine.Pieces;
+using System.Collections.Generic;
+using FluentAssertions;
+using System.Linq;
+
+namespace Chessington.GameEngine.Tests
+{
+    [TestFixture]
+    public class AvailableMovesTest
+    {
+        [Test]
+        public void Squares_WithinBounds()
+        {
+            Board board = new Board();
+            List<Piece> pieces = new List<Piece>(){
+                new Bishop(Player.White),
+                new King(Player.White),
+                new Knight(Player.White),
+                new Pawn(Player.White),
+                new Queen(Player.White),
+                new Rook(Player.White)
+            };
+
+            // place all pieces at top of board
+            for (int i = 0; i < pieces.Count; i++)
+            {
+                board.AddPiece(Square.At(0, i), pieces[i]);
+            }
+
+            // generate available moves for each piece
+            List<Square> moves = new List<Square>();
+            foreach(Piece piece in pieces)
+            {
+                moves.AddRange(piece.GetAvailableMoves(board));
+            }
+
+            // test that all squares are within bounds
+            List<Square> outOfBounds = moves.Where(m => m.Row < 0 || m.Row > 7|| m.Col < 0 || m.Col > 7).ToList();
+            outOfBounds.Should().HaveCount(0);
+        }
+
+
+    }
+}
